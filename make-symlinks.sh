429d0c7c24ea17786713882fd890f866d80a3a26
ngfuong <lenguyenphuong2204@gmail.com>
2025-05-18 02:00:02 +0700

    Add more config files

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -9,15 +9,18 @@
 
 # dotfiles directory
 # edit this with the path to the backup repository
-dir=~/upgraded-dotfiles
+dir=~/dotfiles
 
 # old dotfiles backup directory
 # edit this with the path to make a copy of your dotfiles before running the script
-olddir=~/upgraded-dotfiles/dotfiles_old
+olddir=~/dotfiles/dotfiles_old
 
 # list of files/folders to symlink in homedir
 # edit this with the files you want to backup
-files="bashrc zshrc tmux.conf config/nvim/init.vim config/i3/config"
+# LINUX
+# files="bashrc zshrc tmux.conf config/nvim/init.vim config/i3/config "
+# OSX
+files="zshrc tmux.conf oh-my-zsh config/nvim config/alacritty"
 
 ##########
 
@@ -33,8 +36,8 @@ echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-        echo "Moving any existing dotfiles from ~ to $olddir"
-            mv ~/.$file $olddir/
-                echo "Creating symlink to $file in home directory."
-                    ln -s $dir/$file ~/.$file
+  echo "Moving any existing dotfiles from ~ to $olddir"
+  mv ~/.$file $olddir/
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
 done
